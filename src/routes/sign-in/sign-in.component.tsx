58e8efbad53ead07e55a7b96b250751ae5110550
Joshua Norris <joshua.norris52@gmail.com>
2024-07-28 21:39:52 -0500

    Connecting fields of signup form to state

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
 
 const SignIn: React.FC = () => {
 
     return (
         <>
             <h1>Sign In Page</h1>
+            <SignUpForm />
         </>
     )
 }
